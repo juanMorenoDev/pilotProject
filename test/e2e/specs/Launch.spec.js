6afe1be0e07375d73245617c74d47e59cbf40325
Juanita Triana <juanitatriana@juanitas-mbp.lan>
2019-12-18 15:44:22 -0500

    add-banner-properties

--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -2,7 +2,7 @@ import utils from '../utils'
 
 describe('Launch', function () {
   before(utils.before)
-  //after(utils.after)
+  // after(utils.after)
 
   it('shows the proper application title', function () {
     return this.app.client.getTitle()
@@ -10,21 +10,21 @@ describe('Launch', function () {
         expect(title).to.equal('pilot-project')
       })
   })
-  
+
   it('should detect if the banner is visible', function () {
     const isVisible = this.app.client.$('#banner')
     console.log(isVisible)
     return isVisible.isVisible()
-    .then(banner => {
-      expect(banner).to.equal(true)
-    })
+      .then(banner => {
+        expect(banner).to.equal(true)
+      })
   })
-  
-  it('should give me the banners width', function  () {
+
+  it('should give me the banners width', function () {
     const banner = this.app.client.$('#banner')
     return banner.getElementSize('width')
-    .then(size => {
-      expect(size).to.equal(1120)
-    })
+      .then(size => {
+        expect(size).to.equal(1120)
+      })
   })
 })
